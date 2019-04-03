0ea50ce506c2b377637baabc77a239fec54fc828
JennicaStiehl <jennica.stiehl@gmail.com>
2019-04-03 14:19:54 -0600

    adds items index, only active items

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -3,16 +3,16 @@ RSpec.describe 'As any kind of user on the system' do
   describe "I can visit the items catalog (items)" do
     it 'I see all items in the system except disabled items' do
       sally = User.create(name:"sallyFields", role: 2, password: "salad", email:"f@g.com")
-      item1 = sally.items.create(name:"grapes", inventory: 10, current_price: 1.50, image: "http://kriokrush.com.au/wp-content/uploads/2016/12/ComingSoon.jpg")
+      item1 = sally.items.create(name:"grapes", inventory: 10, current_price: 1.50, active: true, image: "http://kriokrush.com.au/wp-content/uploads/2016/12/ComingSoon.jpg")
       tim = User.create!(name:"timFields", role: 2, password: "salad", email:"fields2@g.com")
-      item2 = tim.items.create(name:"zucchini", inventory: 5, current_price: 2.00, image: "http://kriokrush.com.au/wp-content/uploads/2016/12/ComingSoon.jpg")
-      item3 = sally.items.create(name:"bananas", inventory: 5, current_price: 1.50, image: "http://kriokrush.com.au/wp-content/uploads/2016/12/ComingSoon.jpg")
+      item2 = tim.items.create(name:"zucchini", inventory: 5, current_price: 2.00, active: true, image: "http://kriokrush.com.au/wp-content/uploads/2016/12/ComingSoon.jpg")
+      item3 = sally.items.create(name:"bananas", inventory: 5, current_price: 1.50, active: true, image: "http://kriokrush.com.au/wp-content/uploads/2016/12/ComingSoon.jpg")
       visit items_path
+    
       expect(page).to have_content(sally.name)
       expect(page).to have_content(item1.name)
       expect(page).to have_content(item1.inventory)
       expect(page).to have_content(item1.current_price)
-      # expect(page).to have_xpath("//img[@src='http://kriokrush.com.au/wp-content/uploads/2016/12/ComingSoon.jpg']")
       expect(page).to have_content(item3.name)
       expect(page).to have_content(item3.inventory)
       expect(page).to have_content(item3.current_price)
@@ -20,11 +20,9 @@ RSpec.describe 'As any kind of user on the system' do
       expect(page).to have_content(item2.name)
       expect(page).to have_content(item2.inventory)
       expect(page).to have_content(item2.current_price)
-      # find(:xpath, "//a/img[@alt='#{item2.image}']/..").click
-      # find(:xpath, "//a/img[@alt=''#{item.image}"/..").click click_link("item_path("#{item1.id}")")
-      # click_on item_path|("#{item1.name}")
-      # click_on item_path|("#{item2.image}")
-      # click_on item_path|("#{item2.image}")
+      expect(page).to have_css("img[src*='#{item1.image}']")
+      expect(page).to have_css("img[src*='#{item2.image}']")
+      expect(page).to have_css("img[src*='#{item3.image}']")
     end
   end
 end
--- spec/features/users/navigation_spec.rb
+++ spec/features/users/navigation_spec.rb
@@ -25,8 +25,10 @@ RSpec.describe "navigation" do
     end
   end
   describe 'As a registered user' do
-    xit 'I see the same links as a visitor' do
-      user = User.create(email: "j@gmail.com", password:"password", role:0)
+    it 'I see the same links as a visitor' do
+      user = User.create(email: "j@gmail.com", password:"password", role:0, name:"joey")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit root_path
 
       click_on "Home"
@@ -42,10 +44,10 @@ RSpec.describe "navigation" do
       expect(current_path).to eq(cart_path)
 
       click_on "Logout"
-      expect(current_path).to eq(logout_path)
+      expect(current_path).to eq(root_path)
 
       click_on "Profile"
-      expect(current_path).to eq(profile_path)
+      expect(current_path).to eq(profile_path(user))
       expect(page).to have_content("Logged in as #{user.name}")
     end
   end
