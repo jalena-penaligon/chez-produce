1ff32686abc659b10960a310e3cec88ecf2b4ca1
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-04-03 13:17:41 -0600

    refactored feature test for navigation

--- spec/features/users/navigation_spec.rb
+++ spec/features/users/navigation_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "navigation" do
-  describe "visitor navigation" do
-    it "should have links" do
+  describe "all users navigation" do
+    it "should show all the common links visible to all users" do
       visit root_path
 
       click_on "Home"
@@ -16,6 +16,12 @@ RSpec.describe "navigation" do
 
       click_on "Cart"
       expect(current_path).to eq(cart_path)
+    end
+  end
+
+  describe "visitor navigation" do
+    it "should have links visible only to visitor" do
+      visit root_path
 
       click_on "Login"
       expect(current_path).to eq(login_path)
@@ -25,7 +31,7 @@ RSpec.describe "navigation" do
     end
   end
   describe 'As a registered user' do
-    it 'I see the same links as a visitor except, Register and Login.  I also see link for Profile and Logout' do
+    it 'I see all the links visible only to registered user' do
       user = User.create!(name: 'j', email: "j@gmail.com", password:"password", role:0)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -40,18 +46,6 @@ RSpec.describe "navigation" do
       expect(page).to_not have_content("Register")
       expect(page).to_not have_content("Login")
       
-      click_on "Home"
-      expect(current_path).to eq(root_path)
-
-      click_on "Shop"
-      expect(current_path).to eq(items_path)
-
-      click_on "Merchants"
-      expect(current_path).to eq(merchants_path)
-
-      click_on "Cart"
-      expect(current_path).to eq(cart_path)
-
       click_on "Logout"
       expect(current_path).to eq(root_path)
 
