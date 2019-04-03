2def15a5c8350fbc886366a415ff64ab33a56a51
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-04-03 14:11:34 -0600

    Added additional validations for registration required fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  validates_presence_of :password
+  validates_presence_of :name, :street_address, :city, :state, :zipcode
   validates :email, presence: true, uniqueness: true
   has_many :orders
   has_many :items
+  validates_presence_of :password_digest
+  validates_confirmation_of :password
 
   has_secure_password
+
+  enum role: ['user', 'merchant', 'admin']
 end
