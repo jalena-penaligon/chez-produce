d6ee6699726924c3c619f26507f17764c7f2a9e1
Erin King <44042415+erin-king@users.noreply.github.com>
2019-04-03 16:00:40 -0600

    Add flash message for successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:notice] = "You're logged in!"
       redirect_to profile_path(user)
     else
       render :new
