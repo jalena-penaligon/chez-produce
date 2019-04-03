c0a4e443c8069c4313f5b605661aa2f39b77ae19
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-04-03 16:24:20 -0600

    Created test to display all merchants on index page

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant index page', type: :feature do
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @merchant_3 = create(:inactive_merchant)
+  end
+  describe "When I visit the merchant index page" do
+    it "shows all merchants who are active" do
+      visit merchants_path
+      save_and_open_page
+
+      within "#merchant-#{@merchant_1.id}" do
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_1.city)
+        expect(page).to have_content(@merchant_1.state)
+        expect(page).to have_content(@merchant_1.created_at)
+      end
+
+      within "#merchant-#{@merchant_2.id}" do
+        expect(page).to have_content(@merchant_2.name)
+        expect(page).to have_content(@merchant_2.city)
+        expect(page).to have_content(@merchant_2.state)
+        expect(page).to have_content(@merchant_2.created_at)
+      end
+
+      expect(page).to_not have_content(@merchant_3.name)
+    end
+  end
+end
