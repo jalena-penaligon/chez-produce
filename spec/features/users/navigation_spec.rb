8c8095fe9b437c28033bba18b7f8647f6140b2a9
Erin King <44042415+erin-king@users.noreply.github.com>
2019-04-04 11:08:07 -0600

    Merge branch 'master' of github.com:jalena-penaligon/little_shop_v2 into 13_user_can_log_out

--- spec/features/users/navigation_spec.rb
+++ spec/features/users/navigation_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "navigation" do
-
-  before(:each) do
-  	@user = create(:user)
-  	allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-  end
-
   describe "all users navigation" do
     it "should show all the common links visible to all users" do
 
@@ -28,6 +22,7 @@ RSpec.describe "navigation" do
 
   describe "visitor navigation" do
     it "should have links visible only to visitor" do
+
       visit root_path
 
       click_on "Login"
@@ -35,22 +30,17 @@ RSpec.describe "navigation" do
 
       click_on "Register"
       expect(current_path).to eq(register_path)
+
+      expect(page).to_not have_content("Logout")
     end
   end
 
   describe 'As a registered user' do
-
     it 'I see all the links visible only to registered user' do
-      user = User.create!(name: 'j', email: "j@gmail.com", password:"password", role:0)
-
+      user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit login_path
-
-      fill_in :Email, with: user.email
-      fill_in :Password, with: user.password
-
-      click_on "Login"
+      visit root_path
 
       expect(page).to_not have_content("Register")
       expect(page).to_not have_content("Login")
@@ -62,6 +52,46 @@ RSpec.describe "navigation" do
       expect(current_path).to eq(profile_path)
 
       expect(page).to have_content("Logged in as #{user.name}")
+    end
+  end
+
+  describe "As a merchant user" do
+    it "I see all the links visible only to merchant user" do
+      merchant_1 = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1)
+
+      visit root_path
+
+      expect(page).to_not have_content("Register")
+      expect(page).to_not have_content("Login")
+
+      click_on "Logout"
+      expect(current_path).to eq(root_path)
+
+      click_on "Dashboard"
+      expect(current_path).to eq(dashboard_path)
+
+      expect(page).to have_content("Logged in as #{merchant_1.name}")
+    end
+  end
+
+  describe "As an admin user" do
+    it "I see only the links visible to admin" do
+      admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit root_path
+
+      expect(page).to_not have_content("Register")
+      expect(page).to_not have_content("Login")
+
+      click_on "Logout"
+      expect(current_path).to eq(root_path)
+
+      click_on "Dashboard"
+      expect(current_path).to eq(admin_dashboard_path)
+
+      expect(page).to have_content("Logged in as #{admin.name}")
     end
   end
 end
---
+++ spec/features/users/profile_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'user profile', type: :feature do
+  before(:each) do
+    @user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  describe 'As a registered user' do
+    describe 'When I visit my own profile page' do
+      it 'shows my profile data with a link to edit' do
+
+        visit profile_path(@user)
+
+        expect(page).to have_content(@user.name)
+        expect(page).to have_content(@user.email)
+        expect(page).to have_content(@user.street_address)
+        expect(page).to have_content(@user.city)
+        expect(page).to have_content(@user.state)
+        expect(page).to have_content(@user.zipcode)
+        expect(page).to_not have_content(@user.password)
+
+        click_link("Edit Profile")
+        expect(current_path).to eq(edit_user_path(@user))
+      end
+    end
+  end
+end
