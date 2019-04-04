2651a77597226bbb5109d55ab1290dd888f06373
Erin King <44042415+erin-king@users.noreply.github.com>
2019-04-03 18:35:26 -0600

    Add features/users/logout_spec test for logging out, skipping shopping cart to be addressed later.

---
+++ spec/features/users/logout_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "As a registered user, merchant, or admin" do
+  describe "When I visit the logout path" do
+    it "I am redirected to the welcome/home page of the site and see a flash message indicating I've logged out" do
+
+      visit logout_path
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("You're logged out!")
+    end
+    xit "Any items I had in my shopping cart are deleted" do
+    end
+  end
+end
