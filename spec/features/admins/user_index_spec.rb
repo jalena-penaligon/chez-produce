57e873f1ae33dd5b5f4b85c7f8560c6a7957b8d6
Erin King <44042415+erin-king@users.noreply.github.com>
2019-04-06 10:24:35 -0600

    Add test for admin user index page
    Co-authored-by: Jalena Penaligon <45905026+jalena-penaligon@users.noreply.github.com>

---
+++ spec/features/admins/user_index_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "admin users index", type: :feature do
+  describe "as an admin user" do
+    describe "when I visit user index" do
+      it "shows all users who are not merchants or admins" do
+        user_1 = create(:user)
+        user_2 = create(:user)
+        user_3 = create(:user)
+
+        click_link "Users"
+
+        expect(current_path).to eq(admin_users_path)
+
+        within "user-#{user_1.id}" do
+          expect(page).to have_link(user_1.name)
+          expect(page).to have_content(user_1.created_at)
+          expect(page).to have_button("Upgrade to Merchant")
+        end
+
+        within "user-#{user_2.id}" do 
+          expect(page).to have_link(user_2.name)
+          expect(page).to have_content(user_2.created_at)
+          expect(page).to have_button("Upgrade to Merchant")
+        end
+
+        within "user-#{user_3.id}" do
+          expect(page).to have_link(user_3.name)
+          expect(page).to have_content(user_3.created_at)
+          expect(page).to have_button("Upgrade to Merchant")
+        end
+
+        click_link user_3.name
+
+        expect(current_path).to eq(admin_user_path(user_3))
+      end
+    end
+  end
+end
+
+
+
+
+# I see all users in the system who are not merchants nor admins.
+# Each user's name is a link to a show page for that user ("/admin/users/5")
+# Next to each user's name is the date they registered
+# Next to each user's name is a button that says 'Upgrade to Merchant'
+
+# Only admin users can reach this path.
