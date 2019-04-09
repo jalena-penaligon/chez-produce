73b4d2c033370e3a68c652c1640c373a1b2220cf
JennicaStiehl <jennica.stiehl@gmail.com>
2019-04-08 18:01:50 -0600

    adds story33

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,18 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+  def ship
+    order = Order.find(params[:id])
+    order.update!(status: "shipped")
+    # order.status =
+    redirect_to admin_dashboard_path
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.status = 3
+    @order.save
+    redirect_to admin_dashboard_path
+  end
+
 end
