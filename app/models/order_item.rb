b49979fbb4bd5fa950021b675125b62c1f274aa1
JennicaStiehl <jennica.stiehl@gmail.com>
2019-04-10 14:06:43 -0600

    adds testing

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,26 +2,32 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  def one_day
-    3600*24
+  # def one_day
+  #   3600*24
+  # end
+  #
+  # def fulfillment_time
+  #   ((updated_at - created_at)/one_day).round(2)
+  # end
+  #
+  #
+  # def order_items_by_user(current_user, item_id)
+  #   OrderItem.where(item_id: item_id)
+  # end
+  def self.find_item(item)
+    orderitems = OrderItem.where(item_id: item.id).order(:id)
   end
 
-  def fulfillment_time
-    ((updated_at - created_at)/one_day).round(2)
-  end
-
-
-  def order_items_by_user(current_user, item_id)
-    OrderItem.where(item_id: item_id)
-  end
-
-
-  def self.fulfillment_time(item)
-    orderitems = OrderItem.where(item_id: item.id)
-    the_count = orderitems.count
-    the_sum = orderitems.map do |orderitem|
+  def self.map_it(orderitemsarray)
+    the_sum = orderitemsarray.map do |orderitem|
       (orderitem.updated_at - orderitem.created_at)
     end.sum
+  end
+
+  def self.fulfillment_time(item)
+    orderitems = self.find_item(item)
+    the_count = orderitems.count
+    the_sum = self.map_it(orderitems)
     if the_sum > 0
       avg_time = (the_sum)/the_count
       (avg_time/(3600*24)).round(2)
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -5,25 +5,20 @@ RSpec.describe OrderItem, type: :model do
     @user = create(:user)
     @merchant_1 = create(:merchant)
     @merchant_2 = create(:merchant)
-    @item_1 = create(:item, user: @merchant_1)
+    @item_1 = create(:item, user: @merchant_1, name: "Zucchini")
     @item_2 = create(:item, user: @merchant_2)
+    @item_3 = create(:item, user: @merchant_2)
     @order = create(:order, user: @user)
     create(:order_item, order: @order, item: @item_1, order_price: 1, order_quantity: 1)
     create(:order_item, order: @order, item: @item_2, order_price: 2, order_quantity: 1)
     @order = create(:packaged_order, user: @user)
     @oi_1 = create(:fulfilled_order_item, order: @order, item: @item_1, order_price: 1, order_quantity: 1, created_at: 3.5.days.ago, updated_at: 1.days.ago)
     @oi_2 = create(:fulfilled_order_item, order: @order, item: @item_2, order_price: 2, order_quantity: 1, created_at: 4.5.days.ago, updated_at: 1.days.ago)
+    @oi_3 = create(:fulfilled_order_item, order: @order, item: @item_3, order_price: 2, order_quantity: 1, created_at: 4.5.days.ago, updated_at: 4.5.days.ago)
   end
   describe 'relationships' do
     it { should belong_to :order }
     it { should belong_to :item }
-  end
-
-  describe 'instance methods' do
-    it 'can calc fulfillment time' do
-      actual = @oi_1.fulfillment_time
-      expect(actual).to eq(2.50)
-    end
   end
 
   describe 'class methods' do
@@ -31,6 +26,18 @@ RSpec.describe OrderItem, type: :model do
       actual = OrderItem.fulfillment_time(@item_1)
       expect(actual).to eq(1.25)
     end
-
+    it 'can return a zero from the avg fulfillment_time' do
+      actual = OrderItem.fulfillment_time(@item_3)
+      expect(actual).to eq(0)
+    end
+    it 'can find an item' do
+      actual = OrderItem.find_item(@item_1)
+      expect(actual.count).to eq(2)
+    end
+    it 'can sum an array of date differences' do
+      array = OrderItem.find_item(@item_1)
+      actual = OrderItem.map_it(array)
+      expect(actual.round(2)).to eq(216000.00)
+    end
   end
 end
